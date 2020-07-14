3e4f8a6b4f4c6d74cb0beeb47522582eca890c35
Ulisses Alves Faustino de Oliveira <C0007200@vli-logistica.com.br>
2020-07-14 20:13:58 -0300

    Route

--- portifolio/.nuxt/index.js
+++ portifolio/.nuxt/index.js
@@ -11,7 +11,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_569cccee from 'nuxt_plugin_plugin_569cccee' // Source: .\\components\\plugin.js (mode: 'all')
+import nuxt_plugin_plugin_b3f8530a from 'nuxt_plugin_plugin_b3f8530a' // Source: .\\components\\plugin.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -50,7 +50,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"title":"portifolio","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"## Build Setup"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+    head: {"title":"portifolio","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"```bash\r # install dependencies\r $ npm install"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
     router,
     nuxt: {
@@ -164,8 +164,8 @@ async function createApp(ssrContext, config = {}) {
   }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_569cccee === 'function') {
-    await nuxt_plugin_plugin_569cccee(app.context, inject)
+  if (typeof nuxt_plugin_plugin_b3f8530a === 'function') {
+    await nuxt_plugin_plugin_b3f8530a(app.context, inject)
   }
 
   // Lock enablePreview in context
--- portifolio/.nuxt/router.js
+++ portifolio/.nuxt/router.js
@@ -3,8 +3,8 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _5b874fd4 = () => interopDefault(import('..\\pages\\users\\index.vue' /* webpackChunkName: "pages/users/index" */))
-const _40df7e94 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _74744d76 = () => interopDefault(import('..\\pages\\users\\index.vue' /* webpackChunkName: "pages/users/index" */))
+const _f970d154 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -24,11 +24,11 @@ export const routerOptions = {
 
   routes: [{
     path: "/users",
-    component: _5b874fd4,
+    component: _74744d76,
     name: "users"
   }, {
     path: "/",
-    component: _40df7e94,
+    component: _f970d154,
     name: "index"
   }],
 
