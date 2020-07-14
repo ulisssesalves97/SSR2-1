4ee81f6c6f6aec1a782cc4f96f9aae03f54a713a
Thiago Vieira Reginaldo <thiago.reginaldo@supermix.com.br>
2020-07-13 22:53:21 -0300

    nuxt files

--- portifolio/.nuxt/components/index.js
+++ portifolio/.nuxt/components/index.js
@@ -1,7 +1,9 @@
 export { default as HelloWorld } from '../..\\components\\HelloWorld.vue'
 export { default as Logo } from '../..\\components\\Logo.vue'
 export { default as Bio } from '../..\\components\\profile\\Bio.vue'
+export { default as ProfileFoto } from '../..\\components\\profile\\ProfileFoto.vue'
 
 export const LazyHelloWorld = import('../..\\components\\HelloWorld.vue' /* webpackChunkName: "components_HelloWorld'}" */).then(c => c.default || c)
 export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components_Logo'}" */).then(c => c.default || c)
 export const LazyBio = import('../..\\components\\profile\\Bio.vue' /* webpackChunkName: "components_profile/Bio'}" */).then(c => c.default || c)
+export const LazyProfileFoto = import('../..\\components\\profile\\ProfileFoto.vue' /* webpackChunkName: "components_profile/ProfileFoto'}" */).then(c => c.default || c)
