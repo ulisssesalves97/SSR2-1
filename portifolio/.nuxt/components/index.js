05f11f02bcc2863be269596de137e9f21b1bb8a1
Ulisses Alves Faustino de Oliveira <C0007200@vli-logistica.com.br>
2020-07-14 20:37:46 -0300

    added card

--- portifolio/.nuxt/components/index.js
+++ portifolio/.nuxt/components/index.js
@@ -1,8 +1,10 @@
+export { default as Card } from '../..\\components\\Card.vue'
 export { default as HelloWorld } from '../..\\components\\HelloWorld.vue'
 export { default as Logo } from '../..\\components\\Logo.vue'
 export { default as Bio } from '../..\\components\\profile\\Bio.vue'
 export { default as ProfileFoto } from '../..\\components\\profile\\ProfileFoto.vue'
 
+export const LazyCard = import('../..\\components\\Card.vue' /* webpackChunkName: "components_Card'}" */).then(c => c.default || c)
 export const LazyHelloWorld = import('../..\\components\\HelloWorld.vue' /* webpackChunkName: "components_HelloWorld'}" */).then(c => c.default || c)
 export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components_Logo'}" */).then(c => c.default || c)
 export const LazyBio = import('../..\\components\\profile\\Bio.vue' /* webpackChunkName: "components_profile/Bio'}" */).then(c => c.default || c)
---
+++ portifolio/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Card">
+    <p v-for="habilidade in habilidades" class="item">
+      {{ habilidade }}
+    </p>
+  </div>
+</template>
+<style>
+.item {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  border-radius: 20px;
+  background-color: darkorchid;
+  text-align: center;
+  padding: 70px;
+  margin-top: 10px;
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+}
+</style>
+<script>
+export default {
+  name: 'Card',
+  data() {
+    return {
+      habilidades: ['React', 'Ulisses'],
+    }
+  },
+}
+</script>
